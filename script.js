a6d699e615f3087da90678bd9b51dcfb0d59d2a2
yudin <yudinkarki00@gmail.com>
2023-06-18 22:40:02 +0545

    completed

---
+++ script.js
@@ -0,0 +1,3 @@
+let label = document.querySelectorAll('label').forEach(label =>{
+	label.innerHTML = label.innerText.split('').map((letter, i) => `<span style="transition-delay: ${i * 50}ms">${letter}</span>`).join('');
+});
